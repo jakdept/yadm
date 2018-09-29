e8276c3ab51ce46fdc92dd7550202e7cc5fba4fc
Jack Hayhurst <jakdept@gmail.com>
2018-09-29 00:09:23 -0400

    idk put some stuff together b4 committing and using idk
    Signed-off-by: Jack Hayhurst <jakdept@gmail.com>

--- .yadm/zshrc.d/go.zsh
+++ .yadm/zshrc.d/go.zsh
@@ -1,3 +1,5 @@
+export GOROOT=/usr/local/go #set goroot
+export PATH="${GOROOT}/bin:${PATH}" # add go binarys to path
 export GOPATH=${HOME}/go # set GOPATH
-export PATH="$GOPATH/bin:$PATH" # add my binary to the front of my path
+export PATH="$GOPATH/bin:${PATH}" # add my binary to the front of my path
 export CDPATH=$CDPATH:$GOPATH/src # add GOPATH/src to CDPATH
