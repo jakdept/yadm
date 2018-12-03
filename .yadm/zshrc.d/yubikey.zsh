46aa52722d3ad27d6da54fe10e055840025a6988
Jack Hayhurst <jakdept@gmail.com>
2018-12-03 13:35:12 -0500

    cleaned up the last bit of my yubikey stuff
    Signed-off-by: Jack Hayhurst <jakdept@gmail.com>

--- .yadm/zshrc.d/yubikey.zsh
+++ .yadm/zshrc.d/yubikey.zsh
@@ -1,37 +1,19 @@
-# resets your yubikey back to the current key
-# function yk-reset() {
-# 	# remote all trusted keys, so any pointers to cards are removed
-# 	gpg --with-keygrip --list-secret-keys jakdept@gmail.com 2>/dev/null |
-# 		awk '$1 ~ /Keygrip/ {print $3}' |
-# 		while read keygrip; do
-# 			rm -f ~/.gnupg/private-keys-v1.d/${keygrip}.key
-# 		done
-# 	gpgconf --kill gpg-agent
-# 	kill $SSH_AGENT_PID &>/dev/null
-# 	export GPG_TTY="$(tty)"
-# 	export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
-# 	gpgconf --launch gpg-agent
-# 	# test the gpg agent
-# 	/usr/bin/env gpg --card-status &>/dev/null
-# 	# do something with ssh to get the daemon running
-# 	/usr/bin/env ssh-add -L &>/dev/null
-# 	# set the user.name in git
-# 	/usr/bin/env gpg --card-status |
-# 		awk -F: '/Name of cardholder/ {print $NF}' |
-# 		xargs echo |
-# 		xargs -I{} git config --global user.name "{}"
-# 	/usr/bin/env gpg --card-status |
-# 		awk -F: '/Login Data/ {print $NF}' |
-# 		xargs echo |
-# 		xargs -I{} git config --global user.email "{}"
-# 	echo UPDATESTARTUPTTY | gpg-connect-agent
-# }
-
 export GPG_TTY=$(who | grep $(id -un)|awk '{print "/dev/" $2}')
 export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
 gpgconf --launch gpg-agent
 
-function yk-masterKeys() {
+function yk-reset() {
+	/usr/bin/env gpg --card-status |
+		awk -F: '/Name of cardholder/ {print $NF}' |
+		xargs echo |
+		xargs -I{} git config --global user.name "{}"
+	/usr/bin/env gpg --card-status |
+		awk -F: '/Login Data/ {print $NF}' |
+		xargs echo |
+		xargs -I{} git config --global user.email "{}"
+}
+
+function yk-keys() {
 	gpg --list-keys|tail -n +2|awk '$1 ~ /pub/ {gsub(/.*\//, "", $2); print $2}'
 }
 
