e07545969d293847923a2d7e655ff6a76725290f
Jack Hayhurst <jakdept@gmail.com>
2018-12-04 17:08:49 -0500

    changed path and cdpath, split them from go
    Signed-off-by: Jack Hayhurst <jakdept@gmail.com>

--- .yadm/zshrc.d/go.zsh
+++ .yadm/zshrc.d/go.zsh
@@ -1,5 +1,2 @@
 export GOROOT=/usr/local/go #set goroot
-export PATH="${GOROOT}/bin:${PATH}" # add go binarys to path
 export GOPATH=${HOME}/go # set GOPATH
-export PATH="$GOPATH/bin:${PATH}" # add my binary to the front of my path
-export CDPATH=$CDPATH:$GOPATH/src # add GOPATH/src to CDPATH
---
+++ .yadm/zshrc.d/path.zsh
@@ -0,0 +1,2 @@
+export PATH="${GOPATH}/bin:${GOROOT}/bin:${PATH}" # add go stuff to my path
+export CDPATH="${CDPATH}:${GOPATH}/src:~" # add GOPATH/src to CDPATH
