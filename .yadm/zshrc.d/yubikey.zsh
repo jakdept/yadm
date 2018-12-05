dcd6c7c97d96d6361459fb1ca21373a52f9338db
Jack Hayhurst <jakdept@gmail.com>
2018-12-05 16:28:35 -0500

    fixed yk-reset and formatted yubikey.zsh
    Signed-off-by: Jack Hayhurst <jakdept@gmail.com>

--- .yadm/zshrc.d/yubikey.zsh
+++ .yadm/zshrc.d/yubikey.zsh
@@ -1,31 +1,30 @@
-export GPG_TTY=$(who | grep $(id -un)|awk '{print "/dev/" $2}')
+export GPG_TTY=$(who | grep $(id -un) | awk '{print "/dev/" $2}')
 export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
 gpgconf --launch gpg-agent
 
 function yk-reset() {
-	/usr/bin/env gpg --card-status |
+	/usr/bin/env gpg --card-status 2>/dev/null |
 		awk -F: '/Name of cardholder/ {print $NF}' |
-		xargs echo |
+		sed '/^$/d' |
 		xargs -I{} git config --global user.name "{}"
-	/usr/bin/env gpg --card-status |
-		awk -F: '/Login Data/ {print $NF}' |
-		xargs echo |
+	/usr/bin/env gpg --card-status 2>/dev/null |
+		awk -F: '/Login data/ {gsub(" ", "", $NF);print $NF}' |
+		sed '/^$/d' |
 		xargs -I{} git config --global user.email "{}"
 }
 
 function yk-keys() {
-	gpg --list-keys|tail -n +2|awk '$1 ~ /pub/ {gsub(/.*\//, "", $2); print $2}'
+	gpg --list-keys | tail -n +2 | awk '$1 ~ /pub/ {gsub(/.*\//, "", $2); print $2}'
 }
 
 function yk-ssh() {
-	/usr/bin/env ssh-add -L | pbcopy
+	/usr/bin/env ssh-add -L
 }
 
 function yk-gpg() {
-	yk-keys|xargs -I{} gpg --armor --export {} | pbcopy
+	yk-keys | xargs -I{} gpg --armor --export {}
 }
 
 function yk-encrypt() {
 	gpg --encrypt --armor --recipient
 }
-
